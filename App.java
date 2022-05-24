90a9b857aab43cac5a4da6f260946e45aa460c50
Blair Bristow <blairhbristow@gmail.com>
2022-05-24 10:59:18 -0400

    Rest of formulas

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ class App {
   public static void main(String args[]) {
     mean(scores);
     median(scores);
+    highScore(scores);
+    lowScore(scores);
   }
 
   static double[] scores = { 72, 95, 91, 85, 87, 51, 65, 72, 75, 89 };
@@ -25,5 +27,18 @@ class App {
     else
       median = (double) numericArray[numericArray.length / 2];
     System.out.println("The mean is " + median);
+  }
+
+  private static void highScore(double[] numericArray) {
+    Arrays.sort(numericArray);
+    double length = numericArray.length;
+    double maximum = (int) numericArray[(int) length - 1];
+    System.out.println("The high score is " + maximum);
+  }
+
+  private static void lowScore(double[] numericArray) {
+    Arrays.sort(numericArray);
+    double minimum = numericArray[0];
+    System.out.println("The low score is " + minimum);
   }
 }
